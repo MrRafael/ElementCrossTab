900768674e818ba73bb18239f59570b73cef7ba8
Rafael <mrrafaeldeoliveira@gmail.com>
2024-02-23 14:45:31 -0300

    start

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('topChanged', (value) => {
+    io.emit('top', value)
+  });
+
+  socket.on('leftChanged', (value) => {
+    io.emit('left', value)
+  });
+});
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
